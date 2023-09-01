f37f4e647c6dab1dd8451acd51b6bfd61e02ad41
jayas0001Kanhaiya <kanhaiya.singh@ahom.tech>
2023-09-02 03:55:36 +0530

    TEST-SERIES

--- src/components/common/TestSeriesCard/testSeries.js
+++ src/components/common/TestSeriesCard/testSeries.js
@@ -1,32 +1,37 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../Button/button";
 import "./testSeries.css";
-export const TestSeriesCard = ({data , id , exam , category}) => {
+export const TestSeriesCard = ({ data, id, exam, category }) => {
     const navigate = useNavigate();
+    console.log(data, "Data")
     return (
         <>
-            <div className="test-series-container" onClick={() => navigate(`/testseries/test/${id}`, {state:{category:category , exam: exam}})}>
+            <div className="test-series-container" onClick={() => navigate(`/testseries/test/${id}`, { state: { category: category, exam: exam } })}>
                 <div className="test-series-main-box">
                     <div className="title-box">
-                        <button className="User-count-button"> <img src="/images/Strength.svg"/>{data?.alreadyEnrolled} Users</button>
-                        <h2 className="test-series-title">{data?.title}<br />2023 (Tier I & Tier II)</h2>
-                        <p className="test-series-test-desc">832 Total Tests | {data?.freeTest} Free Tests</p>
+                        <button className="User-count-button"> <img src="/images/Strength.svg" />{data?.alreadyEnrolled} Users</button>
+                        <h2 className="test-series-title" style={{textTransform: "capitalize"}}>{data?.title}<br />2023 (Tier I & Tier II)</h2>
+                        <p className="test-series-test-desc">{data?.totalTest} Total Tests | {data?.freeTest} Free Tests</p>
                     </div>
                     <div className="translate-box">
-                        <p className="translate-language"><img src="/images/translate.svg" alt="translate"/>
-                        {data?.languages?.map((item)=>{
-                            return (item.lanuguage)+","
-                        })}
-                       </p>
+                        <p className="translate-language"><img src="/images/translate.svg" alt="translate" />
+                            {data?.languages?.map((item) => {
+                                return (item.lanuguage) + ","
+                            })}
+                        </p>
                     </div>
                     <ul className="Updated-test-box">
-                        <li>53 New Test Additions</li>
-                        <li>39 Full Test (Tier I)</li>
-                        <li>55 Sectional Test (Tier I)s</li>
+                        {data?.testDivision?.map((data) => {
+                            return (
+                                <>
+                                    <li>{data?.key} ( {data?.value} )</li>
+                                </>
+                            )
+                        })}
                         <span>+685 more tests</span>
                     </ul>
                 </div>
-                <Button value={'View Test Series'} background={'#722E60'} txtColor={'#fff'} borderRadius={'4px'}/>
+                <Button value={'View Test Series'} background={'#722E60'} txtColor={'#fff'} borderRadius={'4px'} />
             </div>
         </>
     )
--- src/components/pages/TestSeries/testSeries.js
+++ src/components/pages/TestSeries/testSeries.js
@@ -1,24 +1,25 @@
 import { Header } from "../../common/Header/header";
 import { Footer } from "../../common/Footer/footer";
-import { UpperHeader } from "../../common/Heading/upperHeader";
-import { About } from "../../common/About/about";
-import { Highlight } from "../../common/Highlight/highlight";
+import { Button } from "../../common/Button/button";
 import { TestSeriesCard } from "../../common/TestSeriesCard/testSeries";
 import { MobileCall } from "../../common/MobileCall/mobileCall";
-import { useLocation , useParams} from "react-router-dom";
-import { useEffect , useState} from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "./testSeries.css";
 import axios from "axios";
 const hightlightListItems = ['Course Highlights Complete coverage of syllabus', 'Focus on building concepts.', 'Bi-weekly doubt resolution session.', '6 Class tests and 2 Full-length mock tests are included.', 'Special sessions for answer writing,']
 export const TestSeries = () => {
     const location = useLocation();
     const params = useParams()
-    console.log(location , "Location")
-    const [testSeriesInfo , setTestSeriesInfo] = useState([]) 
-    const [individualTestInfo , setIndividualTestInfo] = useState({})
+    console.log(location, "Location")
+    const [testSeriesInfo, setTestSeriesInfo] = useState([])
+    const [individualTestInfo, setIndividualTestInfo] = useState({})
+    const [date, setDate] = useState({ date: "", month: "", year: "" })
+    const [language, setLaguage] = useState([])
     const getTestSeriesInfo = async () => {
         try {
             let res = await axios.get(`https://courseselling.onrender.com/api/v1/getAllTest?category=${location?.state?.category}&exam=${location?.state?.exam}`)
-            if(res?.status === 200) {
+            if (res?.status === 200) {
                 setTestSeriesInfo(res?.data)
             }
         } catch (err) {
@@ -29,7 +30,7 @@ export const TestSeries = () => {
         try {
             let res = await axios.get(`https://courseselling.onrender.com/api/v1/Test/${params?.id}`)
             console.log(res)
-            if(res?.status === 200) {
+            if (res?.status === 200) {
                 setIndividualTestInfo(res?.data?.data)
             }
         } catch (err) {
@@ -41,7 +42,28 @@ export const TestSeries = () => {
     useEffect(() => {
         getIndividualTestSeries()
         getTestSeriesInfo()
-    } , [])
+    }, [])
+
+    useEffect(() => {
+        const date = new Date(`${individualTestInfo?.updatedAt}`)
+        const month = date.toLocaleString('default', { month: 'long' });
+        if (date && month) {
+            setDate({ date: date.getDate(), month: month, year: date.getFullYear() })
+        }
+        if (individualTestInfo?.languages) {
+            getLanguage()
+        }
+    }, [individualTestInfo])
+    const getLanguage = () => {
+        console.log(individualTestInfo?.languages, "individualTestInfo?.languages")
+        let language = individualTestInfo?.languages?.map((data, index) => individualTestInfo?.languages?.length - 1 === index ? `${"& " + data?.lanuguage
+            }` : data?.lanuguage)
+        console.log(language, "language")
+        if (language) {
+            setLaguage(language?.join(", "))
+        }
+    }
+    console.log(testSeriesInfo , "testSeriesInfo")
     return (
         <>
             <Header />
@@ -49,8 +71,8 @@ export const TestSeries = () => {
                 <section id="Hero-Section">
                     <div className="main-container">
                         <div className="Left-Box">
-                            <h1>GS Foundation <br />(Prelims Cum Mains)</h1>
-                            <p>By: <b>Snehil Tripathi & Team</b></p>
+                            <h1 style={{ textTransform: "capitalize" }}>{individualTestInfo?.title}<br />(Prelims Cum Mains)</h1>
+                            <p>Last updated on <b>{date?.month + " " + date?.date + ", " + date?.year}</b></p>
                             <div className="Starbox-Container">
                                 <div className="Rating-Box">4.7</div>
                                 <img src="/images/Star.svg" alt="star" />
@@ -58,6 +80,32 @@ export const TestSeries = () => {
                                 <img src="/images/Star.svg" alt="star" />
                                 <img src="/images/Star.svg" alt="star" />
                                 <img src="/images/Star.svg" alt="star" />
+                            </div>
+                            <div className="Individual_Test_Series_Extra_Detail">
+                                <ul>
+                                    <li>
+                                        {individualTestInfo?.totalTest} Total Tests
+                                    </li>
+                                    <li>
+                                        {individualTestInfo?.freeTest} Free Tests
+                                    </li>
+                                    <li>
+                                        11.5K Users
+                                    </li>
+                                    <li>
+                                        {language}
+                                    </li>
+                                </ul>
+                                <ul>
+                                    {individualTestInfo?.testDivision?.map((data) => {
+                                        return (
+                                            <>
+                                                <li>{data?.key} ( {data?.value} )</li>
+                                            </>
+                                        )
+                                    })}
+                                </ul>
+                                <p className="More_Test">+685 more tests</p>
                             </div>
                         </div>
                         <div className="Right-Box">
@@ -74,26 +122,59 @@ export const TestSeries = () => {
                     </section>
                 </section>
                 <section>
-                    <UpperHeader title={'Test Series Description'} />
-                    <About heading={'About Test Series'} about={'The 5-month course covers both papers of Public Administration optional in detail.'} />
-                    <Highlight heading={'Course Highlights'} hightlightListItems={hightlightListItems} />
-                    <div className="course-language">
-                        <h1>Test Series Language</h1>
-                        <ul>
-                            <li>English</li>
-                            <li>Hindi</li>
-                            <li>Public Administration</li>
-                        </ul>
+                    <div className="Ind-Description-Container">
+                        <h1>Test Series Description</h1>
+                        <div className="Ind-Description-Content">
+                            <div className="ind-des-head-box">
+                                <h1>About Test Series</h1>
+                                <p>{individualTestInfo?.about}</p>
+                            </div>
+                            <div className="ind-des-head-box">
+                                <h1>Test Series Highlights</h1>
+                                <ul>
+                                    {individualTestInfo?.highlights?.map((data) => {
+                                        return (
+                                            <>
+                                                <li>{data?.description}</li>
+                                            </>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                            <div className="course-language">
+                                <h1>Test Series Language</h1>
+                                <ul>
+                                    {
+                                        individualTestInfo?.languages?.map((data) => {
+                                            return (
+                                                <>
+                                                    <li>{data?.lanuguage}</li>
+                                                </>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        </div>
                     </div>
+
+
                 </section>
                 <section>
-                    <h1>Explore More Related Test Series</h1>
-                    <div className="Test-Series-Grid">
-                        <TestSeriesCard />
-                        <TestSeriesCard />
-                        <TestSeriesCard />
-                        <TestSeriesCard />
+                    <div className="More_Test_Series_Container">
+                        <h1 className="More_Test_Series_Heading">Explore More Related Test Series</h1>
+                        <div className="Test-Series-Grid">
+                            {testSeriesInfo?.Tests?.map((data) => {
+                                return (
+                                    <>
+                                    <TestSeriesCard data={data} exam={location?.state?.exam} category={location?.state?.category} id={data?._id}/>   
+                                    </>
+                                )
+                            })}
+                        </div>
+                        <Button width={'max-content'} value={'Explore all Test Series'} background={'#fff'} txtColor={'#000'} border={'1px solid #000'} margin={'0 auto'}/>
                     </div>
+
                 </section>
                 <section>
                     <MobileCall />
