bfd054ca42b82f1c16519eea1c2c2c4a981d5598
himanshu jain <himanshu.jain@rupyy.com>
2023-09-01 18:22:07 +0530

    update

--- src/components/pages/Blog/blog.js
+++ src/components/pages/Blog/blog.js
@@ -31,12 +31,8 @@ export const BlogPage = () => {
                             <img src={blog?.image ? blog?.image :"/images/dummy.png"} alt="dummyImage" className="Blog-Page-Image"/>
                         </div>
                         <div className="Indvidual-Blog-Content-Container">
-                            <h1 className="Indvidual-Blog-Content-Heading">What is current research in drought preparation?</h1>
-                            <p className="Indvidual-Blog-Content-Paragraph">Plants need water to grow, but the right amount and timing vary with the type of plant. Climate change and the needs of an exponentially growing population will reduce the amount of water available to produce food.
-                                Plants suffer from drought stress when they do not have sufficient water for their growth and reproduction. Drought stress can cause food crops to become less productive, not produce a crop at all, or even die. Drought stress is a major problem in places where crop production mainly relies upon rainfall to supply crop needs. These areas are called rainfed systems. Rainfed systems account for most of the world’s cultivated land and food production.
-                                The weather cannot be controlled, but to some extent, plants can. Since they cannot move, plants need to adjust and adapt to changes in their environment, such as drought, high temperature, or salt. Scientists are trying to use this ability to help plants respond to stress faster or more effectively to improve production and survival. Although plants do not have brains or a nervous system, they can ‘remember’ the stress after experiencing it multiple times through a process called “stress memory.” The repetition of the stress ‘trains’ the plant to recognize the stress when it happens again.
-                                Plant physiologists – scientists who study how plants function – are trying to use stress memory to improve crops’ tolerance to drought stress. One approach to prepare plants to stress before it occurs is by “priming” them. Although not exactly the same, the idea of priming is like administering a vaccine to the plant.
-                                Just as vaccines give a small amount of a virus to activate our immune system, we expose plants to low levels of stress to activate stress responses. When people get vaccinated, they may experience some symptoms but recover after a short time. Then, if exposed to the virus, that person will have immunity and generally not experience severe symptoms.</p>
+                            <h1 className="Indvidual-Blog-Content-Heading">{blog?.title}</h1>
+                            <p className="Indvidual-Blog-Content-Paragraph">{blog?.content}</p>
                         </div>
                     </div>
                 </section>
