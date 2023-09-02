b0a161a36f7e911d256a11058268659d0e379d7d
himanshu jain <himanshu.jain@rupyy.com>
2023-09-02 21:02:44 +0530

    update

--- src/components/common/TestSeries/testSeries.js
+++ src/components/common/TestSeries/testSeries.js
@@ -7,9 +7,10 @@ import { TestSeriesCard } from "../TestSeriesCard/testSeries";
 import "./testSeries.css";
 import { useEffect } from "react";
 const NavItem =  {'SSC':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"], 'Teaching Exams':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"], 'UPSC':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"], 'civil service':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"], 'GATE':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"], 'AE & JE':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"], 'Goverment Exmas':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"], 'Bamnking & Insurance':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"], 'Degfence Exams':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"], 'Raleway':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"], 'Teaching':["History","Anthropology","Sociology","Maithili","Public Administration","Law","Economics"]};
-export const TestSeriesNav = ({ title , children}) => {
+export const TestSeriesNav = ({ search , setSearch}) => {
     const [category, setCategory] = useState("SSC")
     const [exam, setExam] = useState("History")
+   
     const [testSeries, setTestSeries] = useState([])
     const handleCategory = (data) => {
       setCategory(data) 
@@ -18,9 +19,10 @@ export const TestSeriesNav = ({ title , children}) => {
       setExam(data)
     }
     const testSeriesApi = () => {
-        axios.get(`https://courseselling.onrender.com/api/v1/getAllTest?category=${category}&exam=${exam}`)
+        axios.get(`https://courseselling.onrender.com/api/v1/getAllTest?category=${category}&exam=${exam}&search=${search}`)
             .then(response => {
                 setTestSeries(response.data.Tests);
+
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
