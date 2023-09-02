5727d5dad64fbf27b5d7c6ef70e0d5642c5f218f
jayas0001Kanhaiya <kanhaiya.singh@ahom.tech>
2023-09-03 00:30:45 +0530

    ui changes

--- src/components/pages/Blog/blog.js
+++ src/components/pages/Blog/blog.js
@@ -1,18 +1,21 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import { Header } from "../../common/Header/header";
 import { Footer } from "../../common/Footer/footer";
-import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import LottieControl from "../../common/Loader/loader";
 import "./blog.css"
 export const BlogPage = () => {
     const param = useParams();
     const [blog, setBlog] = useState({})
+    const [loader, setLoader] = useState(false)
     const getBlog = async () => {
         try {
             let res = await axios.get(`https://courseselling.onrender.com/api/v1/blog/${param?.id}`)
-            if(res?.data?.success){
+            if (res?.data?.success) {
                 setBlog(res?.data?.data)
-            } 
+                setLoader(true)
+            }
         } catch (err) {
 
         }
@@ -23,21 +26,36 @@ export const BlogPage = () => {
     }, [])
     return (
         <>
-            <Header />
-            <div className="Course-Detail-Top-Container">
-                <section>
-                    <div className="Indvidual-Blog-Main-Container">
-                        <div className="Indvidual-Blog-ImageContainer">
-                            <img src={blog?.image ? blog?.image :"/images/dummy.png"} alt="dummyImage" className="Blog-Page-Image"/>
+            {
+                !loader ? (
+                    <>
+                        <div className="Loader-Container">
+                            <LottieControl isStopped={false} isPaused={false} />
                         </div>
-                        <div className="Indvidual-Blog-Content-Container">
-                            <h1 className="Indvidual-Blog-Content-Heading">{blog?.title}</h1>
-                            <p className="Indvidual-Blog-Content-Paragraph">{blog?.content}</p>
+                    </>
+                ) : (
+
+                    <>
+                        <Header />
+                        <div className="Course-Detail-Top-Container">
+                            <section>
+                                <div className="Indvidual-Blog-Main-Container">
+                                    <div className="Indvidual-Blog-ImageContainer">
+                                        <img src={blog?.image ? blog?.image : "/images/dummy.png"} alt="dummyImage" className="Blog-Page-Image" />
+                                    </div>
+                                    <div className="Indvidual-Blog-Content-Container">
+                                        <h1 className="Indvidual-Blog-Content-Heading">{blog?.title}</h1>
+                                        <p className="Indvidual-Blog-Content-Paragraph">{blog?.content}</p>
+                                    </div>
+                                </div>
+                            </section>
                         </div>
-                    </div>
-                </section>
-            </div>
-            <Footer />
+                        <Footer />
+
+                    </>
+                )
+            }
+
         </>
     )
 }
--- src/components/pages/teacher.js
+++ src/components/pages/teacher.js
@@ -1,27 +1,28 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
 import { Highlight } from "../common/Highlight/highlight";
 import { About } from "../common/About/about";
-import { Course } from "../common/Course/course";
-import axios from "axios";
-import { useState, useEffect } from "react";
 import { MobileCall } from "../common/MobileCall/mobileCall";
 import { HorizontalCard } from "../common/HorizontalCard/horizontalcard";
 import { Header } from "../common/Header/header";
 import { Footer } from "../common/Footer/footer";
+import LottieControl from "../common/Loader/loader";
+import axios from "axios";
 import "./teacher.css";
-import { useNavigate } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
 //const HightLightList = ["CoFounder of Vidya Education, one of the pioneer institute for BPSC aspirants.", "Eminent Faculty for History and Geography.", "Well Known for his unique teaching style and his motivation lectures among students."]
 export const TeacherDetail = () => {
     const [teacher, setTeacher] = useState()
+    const [HightLightList, setHightLightList] = useState()
+    const [loader, setLoader] = useState(false)
     const navigate = useNavigate();
     const { id } = useParams();
-    const [HightLightList, setHightLightList] = useState()
     const getTeachersApi = () => {
         axios.get(`https://courseselling.onrender.com/api/v1/teacher/${id}`)
             .then(response => {
 
                 setTeacher(response?.data?.data);
                 setHightLightList(response?.data?.data?.highlights)
+                setLoader(true)
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
@@ -34,53 +35,68 @@ export const TeacherDetail = () => {
     }, []);
     return (
         <>
-            <Header />
-            <div className="Teacher-Detail-Container">
-                <div className="teacher-personal-detail">
-                    <img src={teacher?.image} alt="" className="Teacher-Profile-image" />
-                    <div className="basic-detail">
-                        <div>
-                            <h1 className="Teacher-Name">{teacher?.name}<img src="" alt="" /></h1>
-                            <p className="Teacher-Dsignation"><span>{teacher?.designation}</span><span>+1 More</span></p>
+            {
+                !loader ? (
+                    <>
+                        <div className="Loader-Container">
+                            <LottieControl isStopped={false} isPaused={false} />
                         </div>
-                        <div>
-                            <p className="Teacher-Cmn-Dtl"><span><b className="bold">{teacher?.studentsTaught}+</b> Students Taught</span><span><b className="bold">{teacher?.yearsOfExperience}+ Years</b> of Experience</span><span><b className="bold">{teacher?.selections}+</b> Selectors</span></p>
-                        </div>
-                    </div>
-                </div>
-                <About heading={'About Professor'} about={teacher?.about} color={'#722E60'} />
-                <Highlight heading={'Highlights'} hightlightListItems={HightLightList} color={"#722E60"} />
-                <div className="Eductation-Box">
-                    <h1 className="education-head">Education</h1>
 
-                    {teacher?.experiences?.map((item, index) => {
-                        return (
-                            <>
-                                <ul className="education-ul-list" key={index}>
-                                    <li><img /> {item?.institution}</li>
-                                    <li><img /> {item?.year}</li>
-                                </ul >
-                            </>
-                        )
-                    })}
+                    </>
 
-                </div>
-                <div className="Ind-Teacher-Courses">
-                    <h1 className="Heading">Courses By {teacher?.name}</h1>
-                    <div className="teacher-page-course-grid-box">
-                        {teacher?.courses?.map((item , index) => {
-                            return (
-                                <div onClick={() => { navigate(`/course/course-detail/${item?._id}`) }} key={index}>
-                                    <HorizontalCard item={item} />
+                ) : (
+                    <>
+                        <Header />
+                        <div className="Teacher-Detail-Container">
+                            <div className="teacher-personal-detail">
+                                <img src={teacher?.image} alt="" className="Teacher-Profile-image" />
+                                <div className="basic-detail">
+                                    <div>
+                                        <h1 className="Teacher-Name">{teacher?.name}<img src="" alt="" /></h1>
+                                        <p className="Teacher-Dsignation"><span>{teacher?.designation}</span><span>+1 More</span></p>
+                                    </div>
+                                    <div>
+                                        <p className="Teacher-Cmn-Dtl"><span><b className="bold">{teacher?.studentsTaught}+</b> Students Taught</span><span><b className="bold">{teacher?.yearsOfExperience}+ Years</b> of Experience</span><span><b className="bold">{teacher?.selections}+</b> Selectors</span></p>
+                                    </div>
                                 </div>
-                            )
-                        })}
+                            </div>
+                            <About heading={'About Professor'} about={teacher?.about} color={'#722E60'} />
+                            <Highlight heading={'Highlights'} hightlightListItems={HightLightList} color={"#722E60"} />
+                            <div className="Eductation-Box">
+                                <h1 className="education-head">Education</h1>
 
-                    </div>
-                </div>
-                <MobileCall />
-            </div >
-            <Footer />
+                                {teacher?.experiences?.map((item, index) => {
+                                    return (
+                                        <>
+                                            <ul className="education-ul-list" key={index}>
+                                                <li><img /> {item?.institution}</li>
+                                                <li><img /> {item?.year}</li>
+                                            </ul >
+                                        </>
+                                    )
+                                })}
+
+                            </div>
+                            <div className="Ind-Teacher-Courses">
+                                <h1 className="Heading">Courses By {teacher?.name}</h1>
+                                <div className="teacher-page-course-grid-box">
+                                    {teacher?.courses?.map((item, index) => {
+                                        return (
+                                            <div onClick={() => { navigate(`/course/course-detail/${item?._id}`) }} key={index}>
+                                                <HorizontalCard item={item} />
+                                            </div>
+                                        )
+                                    })}
+
+                                </div>
+                            </div>
+                            <MobileCall />
+                        </div >
+                        <Footer />
+                    </>
+                )
+            }
+
         </>
     )
 }
